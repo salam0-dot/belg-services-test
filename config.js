e414b3a783eef9a537411c04d9aa0bf2aaec93bb
salam0-dot <salam03ma@gmail.com>
2026-05-10 15:58:52 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // config.js
 window.env = {
     URL: "https://yljlpvotsysvppimmoxh.supabase.co",
-    KEY: "Sb_publishable_6iHIGUlF5Y9emm6ig238Vg_3he7bc-b"
+    KEY: "sb_publishable_6iHIGUlF5Y9emm6ig238Vg_3he7bc-b"
 };
